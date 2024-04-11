843d2237416c7658e21600e8f9030e1414829f71
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-11 03:04:03 +0200

    checking server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
+app.use('/', (req,res) =>{
+    res.send('Hello from the server')
+})
 app.listen(PORT,() =>{
     console.log(`Server running at port ${PORT}`);
 })
