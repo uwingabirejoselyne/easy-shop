7fcf438226bf69a414f369df9117415578b15e61
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-12 07:11:27 +0200

    registering user route to the main file

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const authRoute = require('./routes/authRoute')
 dbConnect();
+app.use('/api/user',authRoute)
 app.use("/", (req, res) => {
   res.send("Hello from the server");
 });
