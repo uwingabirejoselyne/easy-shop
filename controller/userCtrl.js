f9957b1fa7e2e25e1f40230c93deff3eefd900d3
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-12 16:51:14 +0200

    created error handler

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,19 +1,17 @@
 const User = require('../models/userModel')
-const createUser = async(req,res) =>{
+const asyncHandler = require('express-async-handler')
+const createUser = asyncHandler(async(req,res) =>{
     console.log(req.body);
     const email = req.body.email
     const findUser = await User.findOne({email})
     if(!findUser){
-        const newUser = User.create(req.body)
+        const newUser = await User.create(req.body)
         res.status(201).json({
             msg:"User created"
         })
     }
     else{
-        res.json({
-            msg: "User is already exists",
-            success : false
-        })
+        throw new Error('User already exists')
     }
-}
+})
 module.exports = {createUser}
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
-const authRoute = require('./routes/authRoute')
+const authRoute = require("./routes/authRoute");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 dbConnect();
-app.use(bodyParser())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/user',authRoute)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user", authRoute);
+app.use(notFound);
+app.use(errorHandler);
 app.use("/", (req, res) => {
   res.send("Hello from the server");
 });
