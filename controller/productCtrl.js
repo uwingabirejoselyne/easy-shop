7e31ae65dc98059756a09134cbd9e932b23b74fd
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-16 17:15:56 +0200

    creation of product controller

---
+++ controller/productCtrl.js
@@ -0,0 +1,7 @@
+const Product = require('../models/ProductModel')
+const asyncHandler = require('express-async-handler')
+
+const createProduct = asyncHandler(async(req,res) =>{
+
+})
+module.exports = {createProduct}
