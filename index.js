bfa714b01842f8007b790e69f515238805fd3356
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-17 10:17:51 +0200

    morgan installation

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+
+const morgan = require('morgan')
 const authRoute = require("./routes/authRoute");
 const productRoute = require("./routes/productRoute")
 const cookieParser = require('cookie-parser');
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 dbConnect();
+
+app.use(morgan("dev"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", authRoute);
--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,54 +1,55 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var productSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        unique:true,
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    lowerCase: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  sold: {
+    type: Number,
+  },
+  images: {
+    type: Array,
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  ratings: [
+    {
+      star: Number,
+    //   postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "ser" },
     },
-    slug:{
-        type:String,
-        required:true,
-        unique:true,
-        lowerCase:true
-    },
-    description:{
-        type:String,
-        required:true,
-    },
-    price:{
-        type:Number,
-        required:true,
-    },
-    category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: "Category"
-    },
-    brand:{
-        type: String,
-        enum: ["Apple","Samsung","Lenovo"]
-    },
-    quantity:{
-        price: Number
-    },
-    sold: {
-        type: Number
-    },
-    images:{
-        type: Array
-    },
-    color:{
-        type: String,
-        enum: ["Black","Brown","Red"]
-    },
-    ratings: [
-        {
-            star: Number,
-            postedBy: {type: mongoose.Schema.Types.ObjectId, ref: "ser"}
-        }
-    ]
+  ],
 });
 
 //Export the model
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
