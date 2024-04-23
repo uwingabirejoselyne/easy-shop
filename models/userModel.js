eff5bb28ad85da0f6c9aede96b33008a9cbea6bb
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-23 03:06:07 +0200

    creating a model data for create password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -42,7 +42,10 @@ var userSchema = new mongoose.Schema(
     wishlist: [{ type: Types.ObjectId, ref: "Product" }],
     refreshToken:{
       type:String
-    }
+    },
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
   {
     timestamps: true,
@@ -50,12 +53,21 @@ var userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    next()
+  }
   const salt = await bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+userSchema.methods.createPasswordResetToken = async function (){
+  const resetToken = crypto.randomBytes(32).toString("hex")
+  this.passwordResetToken = crypto.createHash('sha256').update('resettoken').digest("hex")
+  this.passwordChangeAt = Date.now() +30*60*1000;
+  return resetToken
+}
 
 //Export the model
 module.exports = mongoose.model("User", userSchema);
